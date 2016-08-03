d37044198953fecf57b7d679851687e6708cadef
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-08-03 14:16:30 -0300

    Desafio: Validando a descrição.

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -1,11 +1,17 @@
 package com.algaworks.brewer.model;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class Cerveja {
 	@NotBlank
 	private String sku;
+	@NotBlank
 	private String nome;
+	@NotBlank
+	@Size(max = 50)
+	private String descricao;
 
 	public String getSku() {
 		return sku;
@@ -21,5 +27,13 @@ public class Cerveja {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 }
