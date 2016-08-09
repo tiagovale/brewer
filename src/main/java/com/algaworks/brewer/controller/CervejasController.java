f97e2c08910d3961858fe4137450cb20882595d8
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-08-09 15:47:33 -0300

    Baixando e configurando o layout no projeto.

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -25,4 +25,10 @@ public class CervejasController {
 		}
 		return "redirect:/cervejas/novo";
 	}
+
+	@RequestMapping("/cervejas/cadastro")
+	public String cadastro() {
+		return "cerveja/cadastro-produto";
+
+	}
 }
