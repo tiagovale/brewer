c6b0b13e59109135cdb1c6d947d357add22a85bc
Tiago de Oliveira Vale <tiago_oliveira914@hotmail.com>
2016-08-17 23:22:27 -0300

    Prototipando o cadastro de cidade.

--- src/main/java/com/algaworks/brewer/controller/UsuariosController.java
+++ src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -1,20 +1,14 @@
 package com.algaworks.brewer.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.model.Usuario;
 
 @Controller
 public class UsuariosController {
 	@RequestMapping("/usuarios/novo")
-	public String novo(Cerveja cerveja) {
+	public String novo(Usuario usuario) {
 		return "usuario/CadastroUsuario";
 	}
 
