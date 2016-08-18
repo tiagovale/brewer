7c98c8859c9886d823a73e31a9facd8d411fe74b
Tiago de Oliveira Vale <tiago_oliveira914@hotmail.com>
2016-08-17 22:36:30 -0300

    Prototipando o cadastro de usuário.

---
+++ src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -0,0 +1,21 @@
+package com.algaworks.brewer.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.algaworks.brewer.model.Cerveja;
+
+@Controller
+public class UsuariosController {
+	@RequestMapping("/usuarios/novo")
+	public String novo(Cerveja cerveja) {
+		return "usuario/CadastroUsuario";
+	}
+
+}
