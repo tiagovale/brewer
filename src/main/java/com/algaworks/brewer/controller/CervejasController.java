d4b3998cf22a7a59ad138784c09f8334c475db7a
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-08-18 13:45:45 -0300

    Configurando níveis de log com Log4j2.

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,6 +2,7 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
+import org.slf4j.Logger;
 
 @Controller
+
 public class CervejasController {
+	private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
+
 	@RequestMapping("/cervejas/novo")
 	public String novo(Cerveja cerveja) {
 		return "cerveja/CadastroCerveja";
