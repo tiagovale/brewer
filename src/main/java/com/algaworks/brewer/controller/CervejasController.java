d7cfbcd2950edc58513241d04492871edfae5ae9
Tiago de Oliveira Vale <tiago_oliveira914@hotmail.com>
2016-08-17 22:30:54 -0300

    Prototipando o cadastro de cliente.

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,29 +1,15 @@
 package com.algaworks.brewer.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
 
 @Controller
 public class CervejasController {
-	@RequestMapping("/cervejas/novo")
+	@RequestMapping("/clientes/novo")
 	public String novo(Cerveja cerveja) {
-		return "cerveja/CadastroCerveja";
-	}
-
-	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
-		if (result.hasErrors()) {
-			return novo(cerveja);
-		}
-		return "redirect:/cervejas/novo";
+		return "cliente/CadastroCliente";
 	}
 
 }
