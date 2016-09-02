a733879c11d6eb54125518cf7e194d0ce744b1ae
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-09-02 14:26:27 -0300

    Mapeamento da entidade cerveja.

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -1,16 +1,48 @@
 package com.algaworks.brewer.model;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotBlank;
 
+@Entity
+@Table(name = "cerveja")
 public class Cerveja {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+
 	@NotBlank(message = "SKU é obrigatório.")
 	private String sku;
 	@NotBlank(message = "Nome é obrigatório.")
 	private String nome;
-	@Size(min = 1, max = 50,message = "O tamanho da descricao deve estar entre 1 a 50 caracteres.")
+	@Size(min = 1, max = 50, message = "O tamanho da descricao deve estar entre 1 a 50 caracteres.")
 	private String descricao;
+
+	private BigDecimal valor;
+	@Column(name = "teor_alcoolico")
+	private BigDecimal teorAlcoolico;
+	private BigDecimal comissao;
+	@Column(name = "quantidadeEstoque")
+	private Integer quantidadeEstoque;
+	@Enumerated(EnumType.STRING)
+	private Origem origem;
+	@Enumerated(EnumType.STRING)
+	private Sabor sabor;
+	@ManyToOne
+	@JoinColumn(name = "codigo_estilo")
+	private Estilo estilo;
 
 	public String getSku() {
 		return sku;
