d69c85746920885a1af5739178ca0e724a1c8bf1
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-09-21 14:27:49 -0300

    Validando campos da cerveja

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -12,6 +12,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotBlank;
@@ -29,19 +33,29 @@ public class Cerveja {
 	private String sku;
 	@NotBlank(message = "Nome é obrigatório.")
 	private String nome;
-	@Size(min = 1, max = 50, message = "O tamanho da descricao deve estar entre 1 a 50 caracteres.")
+	@Size(max = 50, message = "O tamanho da descricao deve estar entre 1 e 50.")
 	private String descricao;
-
+	@NotNull(message = "O valor é obrigatório")
+	@DecimalMin("0.01")
+	@DecimalMax(value = "9999999.99", message = "O valor da cerveja deve ser menor que R$9.999.999,99")
 	private BigDecimal valor;
+
+	@NotNull(message = "O teor alcóolico é obrigatório")
+	@DecimalMax(value = "100.00", message = "O valor do teor alcóolico deve ser menor que 100")
 	@Column(name = "teor_alcoolico")
 	private BigDecimal teorAlcoolico;
+	@DecimalMax(value = "100.00", message = "A comissão deve ser menor que 100")
 	private BigDecimal comissao;
+	@Max(value = 9999, message = "A quantidade deve ser menor que 9.999")
 	@Column(name = "quantidade_estoque")
 	private Integer quantidadeEstoque;
+	@NotNull(message = "A origem é obrigatória")
 	@Enumerated(EnumType.STRING)
 	private Origem origem;
+	@NotNull(message = "O sabor é obrigatório")
 	@Enumerated(EnumType.STRING)
 	private Sabor sabor;
+	@NotNull(message = "O estilo é obrigatório")
 	@ManyToOne
 	@JoinColumn(name = "codigo_estilo")
 	private Estilo estilo;
