de07c72305be41516fdf4647af0535b4444be1b1
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-09-26 13:25:57 -0300

    Validando se existe um estilo persistido

--- src/main/java/com/algaworks/brewer/controller/EstilosController.java
+++ src/main/java/com/algaworks/brewer/controller/EstilosController.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Estilo;
 import com.algaworks.brewer.service.CadastroEstiloService;
+import com.algaworks.brewer.service.exception.NomeEstiloJaCadastradoException;
 
 @Controller
 public class EstilosController {
@@ -29,6 +30,12 @@ public class EstilosController {
 			RedirectAttributes attributes) {
 
 		if (result.hasErrors()) {
+			return novo(estilo);
+		}
+		try {
+			cadastroEstiloService.salvar(estilo);
+		} catch (NomeEstiloJaCadastradoException e) {
+			result.rejectValue("nome", e.getMessage(), e.getMessage());
 			return novo(estilo);
 		}
 		cadastroEstiloService.salvar(estilo);
