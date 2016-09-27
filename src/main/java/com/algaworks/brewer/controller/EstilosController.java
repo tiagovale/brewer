b79e6f404a53fd1101ffe72026d7984fbad647cc
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-09-27 14:55:35 -0300

    Salvando o estilo com Ajax: diálogo de cadastro rápido.

--- src/main/java/com/algaworks/brewer/controller/EstilosController.java
+++ src/main/java/com/algaworks/brewer/controller/EstilosController.java
@@ -1,13 +1,18 @@
 package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
+import javax.xml.ws.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -43,4 +48,16 @@ public class EstilosController {
 		return new ModelAndView("redirect:/estilos/novo");
 	}
 
+	@RequestMapping(value = "/estilos", method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE })
+	public @ResponseBody ResponseEntity<?> salvar(@RequestBody @Valid Estilo estilo, BindingResult result) {
+		if (result.hasErrors()) {
+			return ResponseEntity.badRequest().body(result.getFieldError("nome").getDefaultMessage());
+		}
+		try {
+			estilo = cadastroEstiloService.salvar(estilo);
+		} catch (NomeEstiloJaCadastradoException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		return ResponseEntity.ok(estilo);
+	}
 }
