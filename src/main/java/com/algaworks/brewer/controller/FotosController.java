1f5e1ef179dad2a6aa7bda367a2038821672d81d
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-10-20 15:45:11 -0200

    Redimensionando a imagem - salvando o thumbnail.

--- src/main/java/com/algaworks/brewer/controller/FotosController.java
+++ src/main/java/com/algaworks/brewer/controller/FotosController.java
@@ -35,4 +35,10 @@ public class FotosController {
 		
 		return fotoStorage.recuperarFotoTemporaria(nome);
 	}
+	
+	@GetMapping("/{nome:.*}")
+	public byte[] recuperar(@PathVariable String nome){
+		
+		return fotoStorage.recuperar(nome);
+	}
 }
