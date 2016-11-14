c1d7b117b7cf5ffcbc2f9afc32289b25c6145858
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-11-14 15:55:39 -0200

    Implementando radio TipoPessoa.

--- src/main/java/com/algaworks/brewer/config/WebConfig.java
+++ src/main/java/com/algaworks/brewer/config/WebConfig.java
@@ -29,6 +29,7 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
 import com.algaworks.brewer.controller.CervejasController;
 import com.algaworks.brewer.controller.converter.EstiloConverter;
 import com.algaworks.brewer.thymeleaf.BrewerDialect;
+import com.github.mxab.thymeleaf.extras.dataattribute.dialect.DataAttributeDialect;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 
@@ -59,8 +60,10 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		SpringTemplateEngine engine = new SpringTemplateEngine();
 		engine.setEnableSpringELCompiler(true);
 		engine.setTemplateResolver(templateResolver());
+
 		engine.addDialect(new LayoutDialect());
 		engine.addDialect(new BrewerDialect());
+		engine.addDialect(new DataAttributeDialect());
 		return engine;
 
 	}
