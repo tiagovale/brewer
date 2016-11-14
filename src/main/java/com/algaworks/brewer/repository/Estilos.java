9fcbba6b0f18961c15605a5a0a0c0e06d58e622f
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-11-14 11:59:37 -0200

    Desafio: Pesquisa de estilos.

--- src/main/java/com/algaworks/brewer/repository/Estilos.java
+++ src/main/java/com/algaworks/brewer/repository/Estilos.java
@@ -6,8 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.brewer.model.Estilo;
+import com.algaworks.brewer.repository.helper.cerveja.EstiloQueries;
 
 @Repository
-public interface Estilos extends JpaRepository<Estilo, Long> {
+public interface Estilos extends JpaRepository<Estilo, Long>,EstiloQueries {
 	public Optional<Estilo> findByNomeIgnoreCase(String nome);
 }
