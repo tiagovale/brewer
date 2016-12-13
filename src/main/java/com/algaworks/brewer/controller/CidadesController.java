20ecedf6897af6ae7a354c9ef49f575830228290
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-12-13 15:00:32 -0200

    Implementando pesquisa de cidades.

--- src/main/java/com/algaworks/brewer/controller/CidadesController.java
+++ src/main/java/com/algaworks/brewer/controller/CidadesController.java
@@ -2,13 +2,17 @@ package com.algaworks.brewer.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,9 +20,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.algaworks.brewer.controller.page.PageWrapper;
+import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.model.Cidade;
+import com.algaworks.brewer.model.Origem;
+import com.algaworks.brewer.model.Sabor;
 import com.algaworks.brewer.repository.Cidades;
 import com.algaworks.brewer.repository.Estados;
+import com.algaworks.brewer.repository.filter.CervejaFilter;
+import com.algaworks.brewer.repository.filter.CidadeFilter;
 import com.algaworks.brewer.service.CadastroCidadeService;
 
 @Controller
@@ -29,7 +39,7 @@ public class CidadesController {
 
 	@Autowired
 	private Estados estados;
-	
+
 	@Autowired
 	private CadastroCidadeService cadastroCidadeService;
 
@@ -45,6 +55,7 @@ public class CidadesController {
 			@RequestParam(name = "estado", defaultValue = "-1") Long codigoEstado) {
 		return cidades.findByEstadoCodigo(codigoEstado);
 	}
+
 	@PostMapping("/nova")
 	public ModelAndView cadastrar(@Valid Cidade cidade, BindingResult result, Model model,
 			RedirectAttributes attributes) {
@@ -54,5 +65,19 @@ public class CidadesController {
 		cadastroCidadeService.salvar(cidade);
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 		return new ModelAndView("redirect:/cidades/nova");
+	}
+
+	@GetMapping
+	public ModelAndView pesquisar(CidadeFilter cidadeFilter, BindingResult result,
+			@PageableDefault(size = 10) Pageable pageable, HttpServletRequest httpServletRequest) {
+		ModelAndView mv = new ModelAndView("cidade/PesquisaCidades");
+		mv.addObject("estados", estados.findAll());
+
+		PageWrapper<Cidade> paginaWrapper = new PageWrapper<>(cidades.filtrar(cidadeFilter, pageable),
+				httpServletRequest);
+		mv.addObject("pagina", paginaWrapper);
+
+		return mv;
+
 	}
 }
--- src/main/java/com/algaworks/brewer/repository/Cidades.java
+++ src/main/java/com/algaworks/brewer/repository/Cidades.java
@@ -3,12 +3,17 @@ package com.algaworks.brewer.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.brewer.model.Cidade;
+import com.algaworks.brewer.repository.filter.CidadeFilter;
 
 public interface Cidades extends JpaRepository<Cidade, Long> {
 	public List<Cidade> findByEstadoCodigo(Long codigoEstado);
 
 	public Optional<Cidade> findByNomeIgnoreCase(String nome);
+
+	public Page<Cidade> filtrar(CidadeFilter cidadeFilter, Pageable pageable);
 }
