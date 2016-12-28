22048444f7ecc2d6895f19097de142cb2e200314
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-12-28 16:36:15 -0200

    Input e componente para Data.

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -53,6 +53,22 @@ Brewer.MaskCep = (function() {
 	return MascaraCep;
 }())
 
+Brewer.MaskDate = (function() {
+	function MaskDate() {
+		this.inputDate = $('.js-date');
+	}
+	MaskDate.prototype.enable = function() {
+		this.inputDate.mask('00/00/0000');
+		this.inputDate.datepicker({
+			orientation:'bottom',
+			language: 'pt-BR',
+			autoclose: true
+			
+		});
+	}
+	return MaskDate;
+}())
+
 $(function() {
 
 	var maskMoney = new Brewer.MaskMoney();
@@ -63,4 +79,7 @@ $(function() {
 
 	var maskCep = new Brewer.MaskCep();
 	maskCep.enable();
+
+	var maskDate = new Brewer.MaskDate();
+	maskDate.enable();
 });
